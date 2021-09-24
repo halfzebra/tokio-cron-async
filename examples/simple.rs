659a20706718ae15cb886ee8291039b6be8829c3
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2021-09-24 17:52:44 +0200

    Rework the example to use slightly cleaner syntax
    Signed-off-by: Eduard Kyvenko <eduard.kyvenko@gmail.com>

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,35 +1,38 @@
-use tokio_cron_async::JobSchedule;
 use chrono::Local;
+use std::error::Error;
 use tokio::time::{sleep, Duration};
+use tokio_cron_async::JobSchedule;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     println!("start {}", Local::now().time());
     let schedule: JobSchedule = JobSchedule::new();
 
-    schedule.add(
-        "1/10 * * * * *",
-        Box::new(|_i| {
-            Box::pin(async move {
-                println!("1/10 {}", Local::now().time());
-                sleep(Duration::from_secs(10)).await;
-            })
-        }),
-    )
-    .await
-    .expect("Added");
+    schedule
+        .add(
+            "1/10 * * * * *",
+            Box::new(|_i| {
+                Box::pin(async move {
+                    println!("1/10 {}", Local::now().time());
+                    sleep(Duration::from_secs(10)).await;
+                })
+            }),
+        )
+        .await?;
 
-    schedule.add(
-        "1/20 * * * * *",
-        Box::new(|_i| {
-            Box::pin(async move {
-                println!("1/20 {}", Local::now().time());
-                sleep(Duration::from_secs(20)).await;
-            })
-        }),
-    )
-    .await
-    .expect("Added");
+    schedule
+        .add(
+            "1/20 * * * * *",
+            Box::new(|_i| {
+                Box::pin(async move {
+                    println!("1/20 {}", Local::now().time());
+                    sleep(Duration::from_secs(20)).await;
+                })
+            }),
+        )
+        .await?;
 
-    tokio::spawn(schedule.run()).await.ok();
+    let _jh = tokio::spawn(schedule.run()).await?;
+
+    Ok(())
 }
